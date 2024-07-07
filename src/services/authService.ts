c90b297be527d01531e52f07063964b900e6426c
sam <samuelabraha@gmail.com>
2024-07-07 23:52:33 +0200

    feat(services): update authService to handle JWT token properly

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -1,11 +1,10 @@
 import axios from 'axios';
 import { AuthCredentials } from '../types/AuthCredentials';
 
-const API_URL = process.env.REACT_APP_AUTH_API_URL;
+const API_URL = import.meta.env.VITE_AUTH_API_URL;
 
 export const signIn = async (credentials: AuthCredentials) => {
-    const response = await axios.post(`${API_URL}/signin`, credentials);
-    document.cookie = `jwt=${response.data.accessToken}; HttpOnly`;
+    const response = await axios.post(`${API_URL}/signin`, credentials, { withCredentials : true });
     return response.data;
 };
 
