e60cffab6f6641c9aaecc017ce5b22f3caec4813
sam <samuelabraha@gmail.com>
2024-07-07 23:04:23 +0200

    feat(components): add Header and Layout

---
+++ src/components/Header.tsx
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom";
+
+export default function Header() {
+    return (
+        <div className="flex justify-between px-4 py-4 bg-stone-800 text-white">
+            <div className="flex items-center font-bold">
+                <h1>Task Manager</h1>
+            </div>
+            <div className="flex items-center space-x-4 font-bold">
+                <Link to="/signin">Sign In</Link>
+                <Link to="/signup">Sign Up</Link>
+            </div>
+        </div>
+    )
+}
