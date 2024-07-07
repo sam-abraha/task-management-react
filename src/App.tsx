9e921a214f64f26675f503f6ebc9d9832251bc9e
sam <samuelabraha@gmail.com>
2024-07-07 23:59:06 +0200

    add routing for Signin, Signup and Homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
+import { Route, Routes } from "react-router-dom"
+import Home from "./pages/Home"
+import Layout from "./components/Layout"
+import SignIn from "./pages/SigninPage"
+import SignUp from "./pages/SignupPage"
 
 function App() {
 
   return (
-    <>
-    <h1 className="font-bold">Task Management App</h1>
-    </>
+    <Routes>
+    <Route path="/" element={<Layout />}>
+      <Route index element={<Home />} />
+      <Route path="/signin" element={<SignIn />} />
+      <Route path="/signup" element={<SignUp />} />
+    </Route>
+    </Routes>
   )
 }
 
