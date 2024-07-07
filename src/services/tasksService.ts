7442f5db0fc337ae60380b6b3303ce8951b62315
sam <samuelabraha@gmail.com>
2024-07-07 23:54:14 +0200

    feat(services): update tasksService to handle task creation and retrieval

---
+++ src/services/tasksService.ts
@@ -0,0 +1,20 @@
+import axios from 'axios';
+import { Task } from '../types/Task';
+import {Cookies} from 'js-cookie'
+
+const API_URL = import.meta.env.VITE_TASKS_API_URL;
+
+const getAuthHeader = () => {
+    const accessToken = Cookies.get('accessToken');
+    return { headers: { Authorization: `Bearer ${accessToken}` } };
+};
+
+export const getTasks = async (): Promise<Task[]> => {
+    const response = await axios.get(API_URL, getAuthHeader());
+    return response.data;
+};
+
+export const createTask = async (title: string, description: string): Promise<Task> => {
+    const response = await axios.post(API_URL, { title, description }, getAuthHeader());
+    return response.data;
+};
