67f4ace58ed2e7bcc18104428522d9f027e45d17
sam <samuelabraha@gmail.com>
2024-07-07 23:49:09 +0200

    feat(components): apply Helvetica font to Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export default function Header() {
     return (
-        <div className="flex justify-between px-4 py-4 bg-stone-800 text-white">
+        <div className="flex justify-between px-4 py-4 bg-stone-800 text-white font-sans">
             <div className="flex items-center font-bold">
                 <h1>Task Manager</h1>
             </div>
