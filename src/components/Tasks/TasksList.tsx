c4e5f84a0cea4e8b60721de8fa8419bba0fb0dfb
sam <samuelabraha@gmail.com>
2024-07-09 23:44:19 +0200

    feat(tasks): add handleTaskDelete method to TasksList

--- src/components/Tasks/TasksList.tsx
+++ src/components/Tasks/TasksList.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { getTasks, createTask } from "../../services/tasksService";
+import { getTasks, createTask, deleteTask } from "../../services/tasksService";
 import { Task, TaskStatus } from "../../types/Task";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSpinner } from "@fortawesome/free-solid-svg-icons";
@@ -12,17 +12,18 @@ export function TasksList() {
     const [loading, setLoading] = useState(true);
 
 
-    const standardizeStatus = (status : TaskStatus) => {
-        switch(status.toUpperCase()) {
+    const standardizeStatus = (status: TaskStatus) => {
+        switch (status.toUpperCase()) {
             case 'TO DO':
-                return 'Done';
+                return 'To Do';
             case 'IN_PROGRESS':
                 return 'In Progress';
-            case 'DONE' :
-                return 'In Progress';
-            default : return 'TO DO';
+            case 'DONE':
+                return 'Done';
+            default:
+                return 'To Do';
         }
-    }
+    };
 
     useEffect(() => {
         const fetchTasks = async () => {
@@ -54,6 +55,19 @@ export function TasksList() {
         } catch (error) {
             console.log('Failed to create task', error);
         }
+    }
+    
+    const handleTaskDelete =  async (e : React.MouseEvent<HTMLButtonElement>) => {
+        const id = e.currentTarget.getAttribute('data-id');
+        if(id) {
+            try {
+                await deleteTask(id);
+                setTasks(tasks.filter(task => task.id !== id));
+            } catch(error) {
+                console.error(error)
+            }
+        }
+
     }
 
     const statuses : TaskStatus[] = ['To Do', 'In Progress', 'Done']
@@ -101,20 +115,24 @@ export function TasksList() {
                         <div key={status} className="flex-shrink-0 w-80">
                              <h3 className="text-xl font-bold mb-4">{status}</h3>
                              {groupedTasks[status].length === 0 ? (
-                                <p className="text-gray-500">Empty</p>
+                                <p className="text-gray-500 text-sm">Empty</p>
                              ) : (
                                 groupedTasks[status].map((task) => (
                                     <div key={task.id} className="bg-white border border-gray-200 p-4 mb-4 shadow-md rounded-lg hover:shadow-lg transition duration-300">
                                         <div className="flex justify-between">
                                             <h3 className="font-bold text-lg hover:text-xl transition duration-500 ease-in-out ">{task.title}</h3>
                                             <div className="flex space-x-4">
-                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="size-5 text-black">
+                                            <button>
+                                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="size-5 text-black">
                                                 <path d="m5.433 13.917 1.262-3.155A4 4 0 0 1 7.58 9.42l6.92-6.918a2.121 2.121 0 0 1 3 3l-6.92 6.918c-.383.383-.84.685-1.343.886l-3.154 1.262a.5.5 0 0 1-.65-.65Z" />
                                                 <path d="M3.5 5.75c0-.69.56-1.25 1.25-1.25H10A.75.75 0 0 0 10 3H4.75A2.75 2.75 0 0 0 2 5.75v9.5A2.75 2.75 0 0 0 4.75 18h9.5A2.75 2.75 0 0 0 17 15.25V10a.75.75 0 0 0-1.5 0v5.25c0 .69-.56 1.25-1.25 1.25h-9.5c-.69 0-1.25-.56-1.25-1.25v-9.5Z" />
-                                            </svg>
-                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="size-5 text-red-500">
+                                                </svg>
+                                            </button>
+                                            <button data-id={task.id} onClick={handleTaskDelete} className="text-red-500">
+                                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="size-5">
                                                 <path d="M6.28 5.22a.75.75 0 0 0-1.06 1.06L8.94 10l-3.72 3.72a.75.75 0 1 0 1.06 1.06L10 11.06l3.72 3.72a.75.75 0 1 0 1.06-1.06L11.06 10l3.72-3.72a.75.75 0 0 0-1.06-1.06L10 8.94 6.28 5.22Z" />
                                                 </svg>
+                                            </button>
                                             </div> 
                                         </div>
                                         <p className="text-gray-600">{task.description}</p>
