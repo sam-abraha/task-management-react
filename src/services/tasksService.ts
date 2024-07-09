6ef11d27fea3febe3de7bb8a08970824f6769295
sam <samuelabraha@gmail.com>
2024-07-09 23:42:38 +0200

    feat(service): add deleteTask method to TasksService

--- src/services/tasksService.ts
+++ src/services/tasksService.ts
@@ -9,12 +9,34 @@ const getAuthHeader = () => {
     return { headers: { Authorization: `Bearer ${accessToken}` } };
 };
 
+
+export const deleteTask = async(id : string) : Promise<void> => {
+    try {
+        await axios.delete(`${API_URL}/${id}`, getAuthHeader());
+        console.log('API',`${API_URL}/${id}`);
+    } catch(error) {
+        console.error('Failed to delete task');
+        throw error;
+    }
+}
+
 export const getTasks = async (): Promise<Task[]> => {
-    const response = await axios.get(API_URL, getAuthHeader());
-    return response.data;
+    try {
+        const response = await axios.get(API_URL, getAuthHeader());
+        return response.data;
+    } catch(error) {
+        console.log(error);
+        throw error;
+    }
+
 };
 
 export const createTask = async (title: string, description: string): Promise<Task> => {
-    const response = await axios.post(API_URL, { title, description }, getAuthHeader());
-    return response.data;
+    try {
+        const response = await axios.post(API_URL, { title, description }, getAuthHeader());
+        return response.data;
+    }catch(error) {
+        console.log(error);
+        throw error;
+    }
 };
