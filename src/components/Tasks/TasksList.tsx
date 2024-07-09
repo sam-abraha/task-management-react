0f2f53a6f1f085dd45585c5b9160234df27f3fa2
sam <samuelabraha@gmail.com>
2024-07-09 18:59:08 +0200

    feat(tasks): add delete and edit button to TasksList

--- src/components/Tasks/TasksList.tsx
+++ src/components/Tasks/TasksList.tsx
@@ -28,7 +28,6 @@ export function TasksList() {
         const fetchTasks = async () => {
             try {
                 const data = await getTasks();
-                // Standardize task status
                 const standardizedTasks = data.map((task) => ({
                     ...task,
                     status: standardizeStatus(task.status) as TaskStatus,
@@ -106,7 +105,18 @@ export function TasksList() {
                              ) : (
                                 groupedTasks[status].map((task) => (
                                     <div key={task.id} className="bg-white border border-gray-200 p-4 mb-4 shadow-md rounded-lg hover:shadow-lg transition duration-300">
-                                        <h3 className="font-bold text-xl">{task.title}</h3>
+                                        <div className="flex justify-between">
+                                            <h3 className="font-bold text-lg hover:text-xl transition duration-500 ease-in-out ">{task.title}</h3>
+                                            <div className="flex space-x-4">
+                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="size-5 text-black">
+                                                <path d="m5.433 13.917 1.262-3.155A4 4 0 0 1 7.58 9.42l6.92-6.918a2.121 2.121 0 0 1 3 3l-6.92 6.918c-.383.383-.84.685-1.343.886l-3.154 1.262a.5.5 0 0 1-.65-.65Z" />
+                                                <path d="M3.5 5.75c0-.69.56-1.25 1.25-1.25H10A.75.75 0 0 0 10 3H4.75A2.75 2.75 0 0 0 2 5.75v9.5A2.75 2.75 0 0 0 4.75 18h9.5A2.75 2.75 0 0 0 17 15.25V10a.75.75 0 0 0-1.5 0v5.25c0 .69-.56 1.25-1.25 1.25h-9.5c-.69 0-1.25-.56-1.25-1.25v-9.5Z" />
+                                            </svg>
+                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="size-5 text-red-500">
+                                                <path d="M6.28 5.22a.75.75 0 0 0-1.06 1.06L8.94 10l-3.72 3.72a.75.75 0 1 0 1.06 1.06L10 11.06l3.72 3.72a.75.75 0 1 0 1.06-1.06L11.06 10l3.72-3.72a.75.75 0 0 0-1.06-1.06L10 8.94 6.28 5.22Z" />
+                                                </svg>
+                                            </div> 
+                                        </div>
                                         <p className="text-gray-600">{task.description}</p>
                                     </div>
                                 ))
