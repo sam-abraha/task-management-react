4ab6ec7119928321adfa9b940254ea1ca40a67ef
sam <samuelabraha@gmail.com>
2024-07-12 01:13:56 +0200

    feat(service): create updateTask method for handling PATCH requests to change the Task status

--- src/services/tasksService.ts
+++ src/services/tasksService.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { Task } from '../types/Task';
+import { Task, TaskStatus } from '../types/Task';
 import Cookies from 'js-cookie'
 
 const API_URL = import.meta.env.VITE_TASKS_API_URL;
@@ -13,9 +13,8 @@ const getAuthHeader = () => {
 export const deleteTask = async(id : string) : Promise<void> => {
     try {
         await axios.delete(`${API_URL}/${id}`, getAuthHeader());
-        console.log('API',`${API_URL}/${id}`);
     } catch(error) {
-        console.error('Failed to delete task');
+        console.error('Failed to delete task',error);
         throw error;
     }
 }
@@ -25,7 +24,7 @@ export const getTasks = async (): Promise<Task[]> => {
         const response = await axios.get(API_URL, getAuthHeader());
         return response.data;
     } catch(error) {
-        console.log(error);
+        console.log('Failed to fetch tasks',error);
         throw error;
     }
 
@@ -36,7 +35,18 @@ export const createTask = async (title: string, description: string): Promise<Ta
         const response = await axios.post(API_URL, { title, description }, getAuthHeader());
         return response.data;
     }catch(error) {
-        console.log(error);
+        console.log('Failed to create a task',error);
         throw error;
     }
 };
+
+
+export const updateTaskStatus = async (id : string, status : TaskStatus) : Promise<Task> => {
+    try {
+        const response = await axios.patch(`${API_URL}/${id}/status`, { status }, getAuthHeader());
+        return response.data;
+    } catch(error) {
+        console.log('Failed to update task',error);
+        throw error;
+    }
+}
