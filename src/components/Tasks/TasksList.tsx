84bbe733f3c460a490d184661ff16c7e9dbf77ba
sam <samuelabraha@gmail.com>
2024-07-12 02:52:19 +0200

    Add logging to handleStatusUpdate method in TasksList

--- src/components/Tasks/TasksList.tsx
+++ src/components/Tasks/TasksList.tsx
@@ -62,6 +62,7 @@ export function TasksList() {
         const nextStatus = getNextStatus(currentStatus);
         try {
             const updatedTask = await updateTaskStatus(id, nextStatus);
+            console.log('Updated task',updatedTask)
             setTasks(tasks.map(task => task.id === id ? { ...task, status: nextStatus } : task));
         } catch (error) {
             console.log('Failed to update task:', error);
@@ -119,7 +120,7 @@ export function TasksList() {
                                             <h3 className="font-bold text-lg hover:text-xl transition duration-500 ease-in-out ">{task.title}</h3>
                                             <div className="flex space-x-4">
                                                 <button className="text-black" onClick={() => handleStatusUpdate(task.id, task.status)}>
-                                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="size-5">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="size-5">
                                                     <path fill-rule="evenodd" d="M10 18a8 8 0 1 0 0-16 8 8 0 0 0 0 16ZM6.75 9.25a.75.75 0 0 0 0 1.5h4.59l-2.1 1.95a.75.75 0 0 0 1.02 1.1l3.5-3.25a.75.75 0 0 0 0-1.1l-3.5-3.25a.75.75 0 1 0-1.02 1.1l2.1 1.95H6.75Z" clip-rule="evenodd" />
                                                     </svg>
                                                 </button>
